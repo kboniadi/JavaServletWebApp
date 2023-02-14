890bd2fc137747303210a57a78497121c5673a3f
HannahJ0 <114255118+HannahJ0@users.noreply.github.com>
2023-02-14 14:09:25 -0800

    another typo

--- src/main/java/com/cs137/javaservletwebapp/service/ProductService.java
+++ src/main/java/com/cs137/javaservletwebapp/service/ProductService.java
@@ -13,7 +13,7 @@ public class ProductService {
                 new Product("img/products/Google-Pixel-7-Pro.png", "Google Pixel 7", "Google", 1000.00, 4.4),
                 new Product("img/products/Samsung-Galaxy-S23-Plus-Lavender.png", "Samsung Galaxy S23 Plus", "Samsung", 1000.00, 4.8),
                 new Product("img/products/La Brique 1.0.png", "La Brique 1.0", "Motorola", 10.00, 5.0),
-                new Product("img/Lime 7 - Pro.png", "Lime 7 - Pro", "Motorola", 30.00, 3.4),
+                new Product("img/products/Lime 7- Pro.png", "Lime 7 - Pro", "Motorola", 30.00, 3.4),
                 new Product("img/products/Doro-750-Consumer-Cellular-Phone.png", "Doro 750", "Doro", 35.00, 3.2));
     }
 }
