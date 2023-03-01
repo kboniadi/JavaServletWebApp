6bcba6f657adbb52745a5b18ffb73d9499f6ab93
Kord Boniadi <kordbon1@gmail.com>
2023-03-01 13:22:36 -0800

    added db connection for employee

--- src/main/java/com/cs137/javaservletwebapp/service/EmployeeService.java
+++ src/main/java/com/cs137/javaservletwebapp/service/EmployeeService.java
@@ -1,15 +1,41 @@
 package com.cs137.javaservletwebapp.service;
 
 import com.cs137.javaservletwebapp.model.Employee;
+import com.cs137.javaservletwebapp.utils.DBSource;
+import com.cs137.javaservletwebapp.utils.PreparedStatementWrapper;
 
-import java.util.Arrays;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeService {
     public List<Employee> getEmployees() {
-        return Arrays.asList(
-                new Employee("img/profile-pic.jpg", "kord", "software engineer", "kord@uci.edu"),
-                new Employee("img/profile-pic.jpg", "hannah", "software engineer", "hannah@uci.edu"),
-                new Employee("img/profile-pic.jpg", "alice", "software engineer", "alice@uci.edu"));
+        String sql = "SELECT * FROM Employee;";
+        List<Employee> employees = new ArrayList<>();
+        try (
+                Connection connection = DBSource.getDataSource().getConnection();
+                PreparedStatementWrapper stat = new PreparedStatementWrapper(connection, sql) {
+                    @Override
+                    protected void prepareStatement(Object... params) throws SQLException {
+                        // nothing
+                    }
+                };
+                ResultSet response = stat.executeQuery();
+        ) {
+            // this will only run if the response has something
+            while (response.next()) {
+                employees.add(new Employee(
+                        response.getString("profileURL"),
+                        response.getString("name"),
+                        response.getString("title"),
+                        response.getString("email")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employees;
     }
 }
